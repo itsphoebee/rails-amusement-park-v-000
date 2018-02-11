1fb97494123733cd635a3d2a4d194a168c9f510f
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 16:49:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
- 
+
    def current_user
      @user = User.find(session[:user_id])
    end
- 
+
    def logged_in?
      !!session[:user_id]
    end
