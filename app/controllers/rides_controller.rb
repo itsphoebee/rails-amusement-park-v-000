573329678fd6289daff5a3b05c40c1b79ec95cbe
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 18:45:49 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,7 +4,7 @@ class RidesController < ApplicationController
   def new
     @ride = Ride.create(ride_params)
     @message = @ride.take_ride
-    redirect_to user_path(@ride.user, :message => @message)
+    redirect_to user_path(@ride.user, notice:@message)
   end
 
   private
