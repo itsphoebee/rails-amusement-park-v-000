2429cd52e5b405a38b349d62d7b12b2ba518fb05
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 17:08:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
    def require_login
      unless logged_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_user_path 
+      redirect_to root_path
     end
   end
 end
