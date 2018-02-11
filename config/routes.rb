d78024b6a44dfb880a8a999b9c5f96d1f1d21703
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 16:41:38 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-
+  root 'application#home'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  resources :users
+  resources :attractions
 
 end
