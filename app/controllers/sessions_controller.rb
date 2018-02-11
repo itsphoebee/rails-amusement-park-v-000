5b58539100518ade13cad8e94c6cfa71930f2ecd
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 17:05:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-     @user = User.find_by(name: params[:user][:name])
-     if @user && @user.authenticate(params[:user][:password])
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-     else
-       render :new
-     end
-   end
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
 
-   def destroy
-     reset_session
-     redirect_to '/'
-   end
+  def destroy
+    reset_session
+    redirect_to '/'
+  end
+ 
 end
