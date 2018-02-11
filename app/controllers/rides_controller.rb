1286949d5f3782f1d3d7377ea10ed35be4ba0e99
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 18:33:49 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,10 +4,10 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.new(ride_params)
     message = @ride.take_ride
-    if @ride.save
-      flash[:notice] = message
-      redirect_to user_path(@ride.user)
-    end
+    @ride.save
+    flash[:notice] = message
+    redirect_to user_path(@ride.user)
+    
   end
 
   private
