60bde8848c1aadce81b5571de92f3517a9691eec
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 17:53:16 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,21 +4,27 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    if attraction.tickets > user.tickets && attraction.min_height > user.height
-      "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
-    elsif
-      attraction.min_height > user.height
-      "Sorry. You are not tall enough to ride the #{attraction.name}."
-    elsif
-      attraction.tickets > user.tickets
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    else
-      #binding.pry
-      user.tickets = user.tickets - attraction.tickets
-      user.nausea = user.nausea + attraction.nausea_rating
-      user.happiness = user.happiness + attraction.happiness_rating
-      user.save
-      "Thanks for riding the #{attraction.name}!"
-    end
+         user = self.user 
+         attraction = self.attraction 
+ 
+         if user.height < attraction.min_height && user.tickets < attraction.tickets 
+             "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+             elsif
+             user.height < attraction.min_height
+             "Sorry. You are not tall enough to ride the #{attraction.name}."
+             elsif 
+             user.tickets < attraction.tickets 
+             "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+ 
+             else 
+             current_tickets = user.tickets - attraction.tickets 
+             current_nausea = user.nausea + attraction.nausea_rating
+             current_happiness = user.happiness + attraction.happiness_rating
+             
+           user.update(tickets: current_tickets, nausea: current_nausea, happiness: current_happiness)
+             "Thanks for riding the #{attraction.name}!"
+         end
+         
+     end
+     
   end
-end
