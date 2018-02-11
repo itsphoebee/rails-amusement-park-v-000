5a5a615d994e2e37455341dc0b3306cd051a42dc
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 17:29:18 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -3,4 +3,14 @@ class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
   end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+    @attraction = Attraction.find_by(:id => params[:id])
+  end
 end
