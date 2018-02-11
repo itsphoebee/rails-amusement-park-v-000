efc4e057817f9465438804e23513ff9de9bb5dc8
Phoebe Lau <phoebelau7@gmail.com>
2018-02-11 17:45:19 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,3 +1,18 @@
 class RidesController < ApplicationController
+  def new
+    @ride = Ride.new
+  end
 
+  def create
+    @ride = Ride.new(ride_params)
+    @notice = @ride.take_ride
+
+    redirect_to user_path(@ride.user), notice: @notice
+  end
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:user_id, :attraction_id)
+  end 
 end
